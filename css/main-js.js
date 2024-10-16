c28a76062ba60867c5555d8a897d00a2903897d3
Jacob-Tinoco <158215329+Jacob-Tinoco@users.noreply.github.com>
2024-10-15 20:18:16 -0600

    Delete css/main-js.js

--- css/main-js.js
+++
@@ -1,9 +0,0 @@
-import Header from '../components/Header.js';
-import Hero from '../components/Hero.js';
-import Footer from '../components/Footer.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    new Header().render('#main-header');
-    new Hero().render('#hero');
-    new Footer().render('#main-footer');
-});
